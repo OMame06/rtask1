56dd4cf49dc6c33021dc96e14c1c25afc26cce3b
Riku Kinjo <sansbook@icoud.com>
2024-12-10 11:05:10 +0900

    test

--- test.bash
+++ test.bash
@@ -11,30 +11,30 @@ res=0
 
 
 #一般的な加算
-out=$(echo "10 + 12"| ./sisoku)
+out=$(echo "10 + 12"| ./calculator)
 [ "${out}" = 22 ]       || ng "$LINENO"
 
 #4,6の置き換え
-out=$(echo "60 + 4"| ./sisoku)
+out=$(echo "60 + 4"| ./calculator)
 [ "${out}" = 77 ]       || ng "$LINENO"
 
 #10未満の自動変換
-out=$(echo "5 + 1"| ./sisoku)
+out=$(echo "5 + 1"| ./calculator)
 [ "${out}" = 0 ]        || ng "$LINENO"
 
 #256を超えた場合の警告文
-out=$(echo "1234 + 5678"| ./sisoku)
+out=$(echo "1234 + 5678"| ./calculator)
 [ "${out}" = error ]    || ng "$LINENO"
 
 #最初に入れた数値と結果で変化が無い場合
-out=$(echo "2 * 2 / 2"| ./sisoku)
+out=$(echo "2 * 2 / 2"| ./calculator)
 [ "${out}" = 変化なし ] || ng "$LINENO"
 
-out=$(echo "12"| ./sisoku)
+out=$(echo "12"| ./calculator)
 [ "${out}" = 変化なし ] || ng "$LINENO"
 
 #数値以外の計算
-out=$(echo "A + B"| ./sisoku)
+out=$(echo "A + B"| ./calculator)
 [ "$?" = 1 ]            || ng "$LINENO"
 [ "${out}" = "" ]       || ng "$LINENO"
 
